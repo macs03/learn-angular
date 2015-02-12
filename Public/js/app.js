cbfe51d3308fe542f02272cc884b98e16fd3e098
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-12 17:06:34 -0430

    agregando uno que otro estilo y algunos tabs para hacerlos funcionales

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,13 +2,14 @@
 	var app = angular.module('pokedex',[]);
 	app.controller('PokemonController',function () {
 		this.pokemon = {
-				id: 001,
+				id: '001',
 				name:'Bulbasaur',
 				species: 'Seed Pokemon',
 				type: ['Grass','Poison'],
 				height:"2'4",
 				weight:"15.2 lbs",
-				abilities: ['Overgrowl','Chlorophyll']
+				abilities: ['Overgrowl','Chlorophyll'],
+				img: 'http://img3.wikia.nocookie.net/__cb20140903033758/pokemon/images/b/b8/001Bulbasaur_Dream.png'
 			};
 	});
 })();
