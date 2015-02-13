fd4fe161c8019b6ed96aeb1447f0654037b314e2
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-13 11:35:38 -0430

    Agregando unos estilos y acomodando la lista

--- Public/js/app.js
+++ Public/js/app.js
@@ -9,7 +9,17 @@
 				height:"2'4",
 				weight:"15.2 lbs",
 				abilities: ['Overgrowl','Chlorophyll'],
-				img: 'http://img3.wikia.nocookie.net/__cb20140903033758/pokemon/images/b/b8/001Bulbasaur_Dream.png'
+				img: 'http://img3.wikia.nocookie.net/__cb20140903033758/pokemon/images/b/b8/001Bulbasaur_Dream.png',
+				stats: {
+					hp: 45,
+					atack: 49,
+					defense: 49,
+					"sp.atack": 65,
+					"sp.defense": 65,
+					speed: 45,
+					total: 318
+				},
+				evolutions: ["Bulbasaur","Ivysaur","Venusaur"]
 			};
 	});
 })();
