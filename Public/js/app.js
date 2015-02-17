337e0fd36fb2efa10c802b3ba0f56c03dedff265
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-17 12:12:12 -0430

    Creando rutas

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,6 +1,17 @@
 	(function () {
 		var app = angular.module('pokedex',[
+			'ngRoute',
 			'pokedex.controllers',
 			'pokedex.directives'
 			]);
+
+		app.config(['$routeProvider',function ($routeProvider) {
+			$routeProvider
+				.when('/',{
+					templateUrl: 'views/pokemon.html',
+					controller: 'PokemonController',
+					controllerAs: 'pkmCtrl'
+				});
+		}]);
+
 	})();
