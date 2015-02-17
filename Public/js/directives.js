ba54f4d0dff345aabe7b3623e25c6a733e4daa87
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-17 11:52:06 -0430

    contruyendo vistas para rutas

---
+++ Public/js/directives.js
@@ -0,0 +1,58 @@
+(function  () {
+	angular.module('pokedex.directives',[])
+		.directive('pokemonData',function  () {
+			return {
+				restric: 'E',
+				templateUrl: 'partials/pokemon-data.html'
+			};
+		})
+		.directive('pokemonName',function  () {
+			return {
+				restric: 'A',
+				templateUrl: 'partials/pokemon-name.html'
+			};
+		})
+		.directive('pokemonImage',function  () {
+			return {
+				restric: 'E',
+				templateUrl: 'partials/pokemon-images.html'
+			};
+		})
+		.directive('pokemonStats',function  () {
+			return {
+				restric: 'E',
+				templateUrl: 'partials/pokemon-stats.html'
+			};
+		})
+		.directive('pokemonEvolutions',function  () {
+			return {
+				restric: 'E',
+				templateUrl: 'partials/pokemon-evolutions.html'
+			};
+		})
+		.directive('pokemonComments',function  () {
+			return {
+				restric: 'E',
+				templateUrl: 'partials/pokemon-comments.html',
+				controller: function  () {
+					this.comments = [];
+					this.comment = {};
+					this.show = false;
+					this.toggle = function  () {
+					this.show = !this.show;
+				};
+				this.anonymousChanged = function  () {
+					if (this.comment.anonymous) {
+						this.comment.email = "";
+					}
+				};
+				this.addComment = function  () {
+					this.comment.date = Date.now();
+					this.comments.push(this.comment);
+					this.comment = {};
+				};
+				},
+				controllerAs: 'cmtsCtrl'
+			};
+		});
+})();
