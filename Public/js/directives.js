35776fc67ef4c8ac27c7f078bc00a35d35d70dfa
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-17 17:03:46 -0430

    Agregando algunos estilos al tipo de pokemon

--- Public/js/directives.js
+++ Public/js/directives.js
@@ -30,6 +30,12 @@
 				templateUrl: 'partials/pokemon-evolutions.html'
 			};
 		})
+		.directive('pokemonType', function () {
+			return {
+				restrict: 'E',
+				templateUrl: 'partials/pokemon-type.html'
+			};
+})
 		.directive('pokemonComments',function  () {
 			return {
 				restric: 'E',
