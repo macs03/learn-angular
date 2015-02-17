eb93ab72325bfeec7498050347900635b96600a2
Miguel Cárdenas <miguelcarde.3@gmail.com>
2015-02-17 10:30:31 -0430

    modularizando la aplicacion

---
+++ Public/js/controllers.js
@@ -0,0 +1,50 @@
+(function  () {
+	angular.module('pokedex.controllers',[])
+		.controller('PokemonController',function () {
+			this.pokemon = {
+					id: '001',
+					name:'Bulbasaur',
+					species: 'Seed Pokemon',
+					type: ['Grass','Poison'],
+					height:"2'4",
+					weight:"15.2 lbs",
+					abilities: ['Overgrowl','Chlorophyll'],
+					stats: {
+						hp: 45,
+						atack: 49,
+						defense: 49,
+						"sp.atack": 65,
+						"sp.defense": 65,
+						speed: 45,
+						total: 318
+					},
+					evolutions: ["Bulbasaur","Ivysaur","Venusaur"]
+				};
+		})
+
+		.controller('TabsController',function () {
+			this.tab = 1;
+			this.selectTab = function  (tab) {
+				this.tab=tab;
+			};
+		});
+		
+		/*.controller('ComentsController', function  () {
+			this.comments = [];
+			this.comment = {};
+			this.show = false;
+			this.toggle = function  () {
+				this.show = !this.show;
+			};
+			this.anonymousChanged = function  () {
+				if (this.comment.anonymous) {
+					this.comment.email = "";
+				}
+			};
+			this.addComment = function  () {
+				this.comment.date = Date.now();
+				this.comments.push(this.comment);
+				this.comment = {};
+			};
+		});*/
+})();
